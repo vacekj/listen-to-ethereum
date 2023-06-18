241291eb4f3e1a8ebb07badace2bbeba12de54d5
Atris <vacekj@outlook.com>
2023-06-18 22:49:58 +0200

    chore: refactor

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Listen to Ethereum
 
 Inspired by [Listen to Wikipedia](https://listen.hatnote.com)
+
+todo list:
+
+- [x] volume slider
+- [x] fix block sound (doesn't play rn)
+- [ ] chain selector
+- [ ] mute/unmute by clicking on volume button
+- [ ] debouncing / anticrash => sound queue system, delay at least 50ms of each other
+- [ ] better animation
+- [ ] blobs are clickable to tx explorer
+- [ ] nicer UI
+- [ ] short explainer a- [ la hatnote
+- [ ] smol tps meter
+- [ ] txs, blocks listened count
+- [ ] google analytics
+- [ ] OpenGraph for twitter sharing
+
+then launch on Twitter and Product Hunt for the lulz
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,7 +1,8 @@
 import { useAudioEnabled, useVolumeStore } from "@/hooks";
+import { TxBlob } from "@/TxBlob";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import { ActionIcon, Box, Container, Flex, Group, Header, Slider, Title, useMantineColorScheme } from "@mantine/core";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { useCallback, useEffect, useState } from "react";
 import { useAudioPlayer } from "react-use-audio-player";
 import { Block, createPublicClient, Hash, Hex, http } from "viem";
@@ -118,99 +119,4 @@ export function Canvas() {
       </div>
     </>
   );
-}
-
-function TxBlob(props: {
-  hash: string;
-  confirmed: boolean;
-  shouldPlaySounds: boolean;
-}) {
-  const { load, play, setVolume } = useAudioPlayer();
-  const { volume } = useVolumeStore();
-  useEffect(() => {
-    setVolume(volume);
-  }, [volume]);
-
-  const audioEnabled = useAudioEnabled();
-  useEffect(() => {
-    if (props.confirmed) {
-      return;
-    }
-    load(
-      props.confirmed
-        ? `/sounds/celesta/c${Math.round(stringToNumberInRange(props.hash, 1, 27)).toString().padStart(3, "0")}.mp3`
-        : `/sounds/clav/c${Math.round(stringToNumberInRange(props.hash, 1, 27)).toString().padStart(3, "0")}.mp3`,
-    );
-  }, []);
-
-  useEffect(() => {
-    if (props.shouldPlaySounds) {
-      play();
-    }
-  }, [audioEnabled]);
-
-  const color = stringToColour(props.hash);
-  return (
-    <motion.div
-      transition={{
-        duration: 0.5,
-        ease: "anticipate",
-      }}
-      exit={{
-        opacity: 0,
-        scale: 0,
-      }}
-      initial={{
-        opacity: 0,
-        scale: 0.9,
-      }}
-      animate={{
-        opacity: props.confirmed ? 1 : 0.3,
-        scale: 1,
-      }}
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        overflow: "hidden",
-        height: 100,
-        width: 100,
-        borderRadius: 50,
-        background: color,
-        position: "absolute",
-        top: stringToNumberInRange(props.hash, 100, window.innerHeight - 200),
-        left: stringToNumberInRange(props.hash, 100, window.innerWidth - 200),
-      }}
-    >
-      {props.hash.substring(0, 9)}
-    </motion.div>
-  );
-}
-
-const stringToColour = function(str: string): string {
-  let i;
-  let hash = 0;
-  for (i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  let colour = "#";
-  for (i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xFF;
-    colour += ("00" + value.toString(16)).substr(-2);
-  }
-  return colour;
-};
-
-function stringToNumberInRange(input: string, min: number, max: number): number {
-  // Hash the input string
-  let hash = 0;
-  for (let i = 0; i < input.length; i++) {
-    hash = (hash << 5) - hash + input.charCodeAt(i);
-    hash |= 0; // Convert to 32-bit integer
-  }
-
-  // Map the hash value to the desired range
-  const range = max - min;
-
-  return (Math.abs(hash) % (range + 1)) + min;
 }
