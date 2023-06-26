0fc24d7f2829c9b37a2d2eb73dea43ed22f55831
Atris <vacekj@outlook.com>
2023-06-26 23:12:23 +0200

    feat: hotjar and google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import VitePluginRadar from "vite-plugin-radar";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-PGN78BN7B9",
+      },
+      hotjar: {
+        id: 3550054,
+      },
+    }),
+  ],
 });
