bba29c0d7a470132ca3c875932f9d3b2a182b0d0
Atris <vacekj@outlook.com>
2023-06-26 23:28:26 +0200

    feat: add hero and og

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import ogPlugin from "vite-plugin-open-graph";
 import VitePluginRadar from "vite-plugin-radar";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    ogPlugin({
+      basic: {
+        title: "Listen to Ethereum",
+        description: "Listen to a generative music stream of the Ethereum blockchain",
+        url: "https://listen.atris.cc",
+        image: "https://listen.atris.cc/hero.webp",
+      },
+    }),
     react(),
     tsconfigPaths(),
     VitePluginRadar({
